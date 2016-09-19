fc1bb03fcb591a6c58726df470be27e7bb02b909
Kathleen <kathleenbryan24@gmail.com>
2016-09-19 10:01:37 -0700

    fix 2

--- CreativeCoding_Assignment1/630/sketch.js
+++ CreativeCoding_Assignment1/630/sketch.js
@@ -1,29 +1,29 @@
 function setup() {
   
   createCanvas(500,500);
-  background(0); 
+  background(255,0,255); 
 }
 
 function draw() {
   for (var x = 0; x < 250 ; x +=2) {
-  line(0, mouseX +2 , 500, mouseX 2 );
-  stroke(0,255,0); 
+  line(0, x +2 , 500, x +2 );
+  stroke(255,255,255); 
   }
 
  for (var x =1; x < 250 ; x +=2) {
-  line(0, mouseX+2, 500, mouseX+2);
-    stroke(255,255,0);
+  line(0, x+2, 500, x+2);
+    stroke(0,0,0);
   }
 
 
  for (var x = 0; x < 500 ; x +=4) {
   line(x+2, 250, x+2, 500 );
-  stroke(255,255,0); 
+  stroke(255,255,255); 
   }
   
   for (var x = 1; x < 500 ; x +=4) {
   line(mouseX +2, 250, mouseX+2, 500 );
-  stroke(255); 
+  stroke(0,0,0); 
   }
 
 /*
