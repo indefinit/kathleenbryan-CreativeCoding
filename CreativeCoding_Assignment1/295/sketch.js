c8cc0f90e564c7de48ad94d0739d46c830b0362a
Kathleen <kathleenbryan24@gmail.com>
2016-09-19 09:41:27 -0700

    assignment1

---
+++ CreativeCoding_Assignment1/295/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+createCanvas(700, 700);
+background(0);
+
+
+}
+
+function draw() {
+stroke(255, 255, 255);
+strokeWeight(4);
+noFill(); 
+rect(20, 20, 60, 60);
+triangle(135, 180, 163, 125, 191, 180);
+rect(20, 250, 60, 60);
+triangle(235, 280, 263, 225, 291, 280);
+rect(20, 450, 60, 60);
+triangle(335, 380, 363, 325, 391, 380);
+}
+
